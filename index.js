5b3f55fddef30ed8312bfa66ec12f4c6049059b2
Siftab <siiffuuu@gmail.com>
2024-03-06 22:30:41 +0600

    api for user deletation implemented

--- index.js
+++ index.js
@@ -82,9 +82,16 @@ const client = new MongoClient(uri, {
       })
 
       // User APis 
+
       app.get('/users',async(req,res)=>{
         const result = await userCollection.find().toArray();
         res.send(result)
+      })
+      app.delete('/users/:id',async(req,res)=>{
+        const id= req.params.id;
+        console.log("deleting user ",id)
+        const result= await userCollection.deleteOne({_id: new ObjectId(id)})
+        res.send(result )
       })
       app.post('/user',async(req,res)=>{
         const user = req.body;
